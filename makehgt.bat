f4117bb45282abe999c32c64bab23edaafb7c9c3
Terry Murphy <vendor@tpmassoc.com>
2026-01-10 12:43:22 -0800

    Update makehgt.bat
    Changed flags

--- makehgt.bat
+++ makehgt.bat
@@ -1,10 +1,10 @@
-gcc -Wall -Wextra -pedantic-errors -std=c99 -posix -c hgtInit.c 
-gcc -Wall -Wextra -pedantic-errors -std=c99 -posix -c ThetaOfT.c 
-gcc -Wall -Wextra -pedantic-errors -std=c99 -posix -c GramAtN.c 
-gcc -Wall -Wextra -pedantic-errors -std=c99 -posix -c GramNearT.c 
-gcc -Wall -Wextra -pedantic-errors -std=c99 -posix -c RSbuildcoeff.c 
-gcc -Wall -Wextra -pedantic-errors -std=c99 -posix -c RSremainder.c 
-gcc -Wall -Wextra -pedantic-errors -std=c99 -posix -c RSmainTerm.c 
-gcc -Wall -Wextra -pedantic-errors -std=c99 -posix -c HardyZcalc.c 
+gcc -Wall -Wextra -pedantic-errors -std=gnu17 -pthread -c hgtInit.c 
+gcc -Wall -Wextra -pedantic-errors -std=gnu17 -pthread -c ThetaOfT.c 
+gcc -Wall -Wextra -pedantic-errors -std=gnu17 -pthread -c GramAtN.c 
+gcc -Wall -Wextra -pedantic-errors -std=gnu17 -pthread -c GramNearT.c 
+gcc -Wall -Wextra -pedantic-errors -std=gnu17 -pthread -c RSbuildcoeff.c 
+gcc -Wall -Wextra -pedantic-errors -std=gnu17 -pthread -c RSremainder.c 
+gcc -Wall -Wextra -pedantic-errors -std=gnu17 -pthread -c RSmainTerm.c 
+gcc -Wall -Wextra -pedantic-errors -std=gnu17 -pthread -c HardyZcalc.c 
 ar rcs libhgt.a hgtInit.o ThetaOfT.o GramAtN.o GramNearT.o RSbuildcoeff.o RSremainder.o RSmainTerm.o HardyZcalc.o
 
